5b2dbae95b072cbf67a6a4fc61c78a61256f5ccc
Victor Palmero <victor.palmero@undostres.com.mx>
2024-07-19 19:09:04 -0400

    feat: add swagger schedule and configure application properties

---
+++ src/main/java/com/palmerodev/storage_service_spr/config/Schedules.java
@@ -0,0 +1,20 @@
+package com.palmerodev.storage_service_spr.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class Schedules {
+
+    @Value("${app.storage-service.url}")
+    String SERVER_URL;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        log.info("Swagger URL: {}/swagger-ui/swagger-ui.html", SERVER_URL);
+    }
+}
