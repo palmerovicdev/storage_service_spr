0c1c29040cab6261bb8421a920729c8af1fc71e4
Victor Palmero <victor.palmero@undostres.com.mx>
2024-07-19 18:55:46 -0400

    feat: initial commit

---
+++ src/main/java/com/palmerodev/storage_service_spr/StorageServiceSprApplication.java
@@ -0,0 +1,13 @@
+package com.palmerodev.storage_service_spr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageServiceSprApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageServiceSprApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/palmerodev/storage_service_spr/StorageServiceSprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palmerodev.storage_service_spr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceSprApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
