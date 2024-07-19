152e8a84fbf85532390dbb73257483341972d15a
Victor Palmero <victor.palmero@undostres.com.mx>
2024-07-19 19:01:35 -0400

    feat: add swagger and openapi support

---
+++ src/main/java/com/palmerodev/storage_service_spr/config/OpenAPIConfiguration.java
@@ -0,0 +1,33 @@
+package com.palmerodev.storage_service_spr.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfiguration {
+
+   @Bean
+   public OpenAPI defineOpenApi() {
+       var server = new Server();
+       server.setDescription("Fake Storage API Server. A simple API to manage categories, products and files.");
+
+       var myContact = new Contact();
+       myContact.setName("Victor Palmero Valdes");
+       myContact.setEmail("palmerodev@gmail.com");
+       myContact.setUrl("https://github.com/palmerovicdev");
+
+       var information = new Info()
+               .title("Fake Storage API")
+               .version("1.0")
+               .license(new io.swagger.v3.oas.models.info.License().name("MIT").url("https://opensource.org/licenses/MIT"))
+               .description("This API exposes endpoints to manage categories, products and files.")
+               .contact(myContact);
+       return new OpenAPI().info(information).servers(List.of(server));
+   }
+}
