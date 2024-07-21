6f7935ddf1de80f3ac0cce7863af01b78baf2e86
Victor Palmero <victor.palmero@undostres.com.mx>
2024-07-21 11:16:13 -0400

    feat: add openAPI documentation

--- src/main/java/com/palmerodev/storage_service_spr/controller/StorageController.java
+++ src/main/java/com/palmerodev/storage_service_spr/controller/StorageController.java
@@ -2,6 +2,12 @@ package com.palmerodev.storage_service_spr.controller;
 
 import com.palmerodev.storage_service_spr.model.request.UploadImageRequest;
 import com.palmerodev.storage_service_spr.service.StorageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,18 +23,31 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/storage")
 @RequiredArgsConstructor
+@Tag(name = "Storage", description = "Storage controller")
 public class StorageController {
     private final StorageService storageService;
 
+    @Operation(summary = "Download an image", description = "Download an image from DB", responses = {
+            @ApiResponse(responseCode = "200", description = "Image downloaded successfully"),
+            @ApiResponse(responseCode = "404", description = "Image not found")
+    }, method = "GET")
     @GetMapping("/{name}")
-    public ResponseEntity<?> download(@PathVariable String name) {
+    public ResponseEntity<?> download(
+            @Parameter(description = "Image name", required = true)
+            @PathVariable String name) {
         var imageData = storageService.downloadImage(name);
         if (Objects.isNull(imageData)) return ResponseEntity.notFound().build();
         return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/" + imageData.getType())).body(imageData.getImage());
     }
 
+    @Operation(summary = "Store a new image", description = "Store a new image in DB", responses = {
+            @ApiResponse(responseCode = "201", description = "Image stored successfully"),
+            @ApiResponse(responseCode = "500", description = "Error storing image")
+    }, method = "POST")
     @PostMapping
-    public ResponseEntity<?> upload(@RequestBody UploadImageRequest file) {
+    public ResponseEntity<?> upload(
+            @Parameter(description = "Upload image request", required = true)
+            @RequestBody UploadImageRequest file) {
         String imageName;
         try {
             imageName = storageService.uploadImage(file);
